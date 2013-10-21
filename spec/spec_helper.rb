b5be70e3852ea421051bd8b6c2b536d4c6f3698a
lsnyder <AGarlandAsst@caa.com>
2013-10-21 14:59:40 -0700

    few

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,11 @@ require 'spork'
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
 
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start 'rails'
+
 Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
